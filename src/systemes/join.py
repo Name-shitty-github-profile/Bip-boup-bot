3a7f1f6e0f93197c2258331b7a10ff69f5ff33b5
Noémie <102274923+Name-shitty-github-profile@users.noreply.github.com>
2023-01-17 15:26:24 -0500

    Create join.py

---
+++ src/systemes/join.py
@@ -0,0 +1,28 @@
+from nextcord.ext import commands
+import nextcord
+from database import add, delete, search
+
+class Join(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+
+  @nextcord.slash_command(name = "bienvenue", description = "Activer le système de bienvenu sur le serveur")
+  async def Join_slash(self, interaction: nextcord.Interaction, channel: nextcord.TextChannel = nextcord.SlashOption(name="salon", description="Le salon que ou tu veux les bienvenues"), status: bool = nextcord.SlashOption(name="status", description="Activer ou non?")):
+    if status is None or status is False:
+      delete({"_id": interaction.guild.id}, 'join')
+      await interaction.response.send_message("Le système de bienvenue n'est plus activé !")
+      return  
+    add({"_id": interaction.guild.id, "id": channel.id}, 'join')
+    await interaction.response.send_message("Le système de bienvenue est activé !")
+    await interaction.channel.send("Test en cours d'envoie !")
+    await self.bot.get_channel(channel.id).send(embed=nextcord.Embed(title='Bienvenu(e) test !', description=f"Passe un bon moment sur {interaction.guild.name} !", color=0x2ecc71))
+    
+  @commands.Cog.listener("on_member_join")
+  async def Join_systeme(self, member):
+    data = search(member.guild.id, 'join')
+    if data is None:
+      return
+    await self.bot.get_channel(data['id']).send(embed=nextcord.Embed(title=f"Bienvenu(e) {member.name} !", description=f"Passe un bon moment sur {member.guild.name} !", color=0x2ecc71))
+
+def setup(bot):
+  bot.add_cog(Join(bot))
